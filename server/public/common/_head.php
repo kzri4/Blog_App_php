575f5785ecc0713eb721e37c0e6ae62c828e72a2
kzri4 <kazu4126ab@gmail.com>
2021-06-22 09:15:42 +0900

    更新画面の作成

---
+++ server/public/common/_head.php
@@ -0,0 +1,8 @@
+    <head>
+        <meta charaset="UTF-8">
+        <meta name="viewport" contetnt="width=device-width, initial-scale=1.0">
+        <link rel ="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+        <link rel="stylesheet" href="/css/style.css">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
+        <title>ブログアプリ</title>
+    </head>
--- server/public/users/new.php
+++ server/public/users/new.php
@@ -1,13 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charaset="UTF-8">
-        <meta name="viewport" contetnt="width=device-width, initial-scale=1.0">
-        <link rel ="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
-        <link rel="stylesheet" href="/css/style.css">
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&family=Sawarabi+Mincho&display=swap" rel="stylesheet">
-        <title>ブログアプリ</title>
-    </head>
+    <?php include_once __DIR__ . '/../common/_head.php' ?>
 
     <body>
         <?php include_once __DIR__ . '/../common/_header.php'?>
